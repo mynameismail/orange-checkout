e7d821e13491a68088d36388e19e1a1867a9c5f3
Ismail <ismailsmakhus@gmail.com>
2021-01-19 14:11:34 +0700

    add vuex-pesistedstate module

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -46,6 +47,5 @@ export default new Vuex.Store({
       state.payment = payload
     },
   },
-  actions: {
-  },
+  plugins: [createPersistedState({ storage: window.sessionStorage })],
 })
