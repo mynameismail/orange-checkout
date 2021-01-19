e50205fb60e3ee52b7c40f934b7231d818537aa3
Ismail <ismailsmakhus@gmail.com>
2021-01-19 12:54:30 +0700

    prepare some states and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentPage: 'delivery',
+    asDropshipper: false,
+    shipmentType: {
+      'gosend': {
+        name: 'GO-SEND',
+        price: 15000,
+        when: 'today'
+      },
+      'jne': {
+        name: 'JNE',
+        price: 9000,
+        when: '2 days'
+      },
+      'personal': {
+        name: 'Personal Courier',
+        price: 29000,
+        when: '1 day'
+      }
+    },
+    paymentType: {
+      'ewallet': 'e-Wallet',
+      'bank': 'Bank Transfer',
+      'va': 'Virtual Account'
+    },
+    shipment: null,
+    payment: null,
   },
   mutations: {
+    setAsDropshipper(state, payload) {
+      state.asDropshipper = payload
+    },
+    setCurrentPage(state, payload) {
+      state.currentPage = payload
+    },
+    setShipment(state, payload) {
+      state.shipment = payload
+    },
+    setPayment(state, payload) {
+      state.payment = payload
+    },
   },
   actions: {
   },
-  modules: {
-  }
 })
