8a7b746fbacf609f5584b782b38d0865749e3ce0
Ismail <ismailsmakhus@gmail.com>
2021-01-19 12:55:58 +0700

    implement shipment and payment

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="shipment">
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-gosend" name="shipment" value="gosend">
+        <input type="radio" class="choice__input" id="radio-gosend" name="shipment" value="gosend" v-model="shipment">
         <label class="choice__label" for="radio-gosend">
           <p>
             <span class="text">GO-SEND</span><br>
@@ -16,7 +16,7 @@
         <span class="choice__check material-icons">check</span>
       </div>
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-jne" name="shipment" value="jne">
+        <input type="radio" class="choice__input" id="radio-jne" name="shipment" value="jne" v-model="shipment">
         <label class="choice__label" for="radio-jne">
           <p>
             <span class="text">JNE</span><br>
@@ -26,7 +26,7 @@
         <span class="choice__check material-icons">check</span>
       </div>
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-personal" name="shipment" value="personal">
+        <input type="radio" class="choice__input" id="radio-personal" name="shipment" value="personal" v-model="shipment">
         <label class="choice__label" for="radio-personal">
           <p>
             <span class="text">Personal Courier</span><br>
@@ -42,7 +42,7 @@
     </div>
     <div class="payment">
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-ewallet" name="payment" value="ewallet">
+        <input type="radio" class="choice__input" id="radio-ewallet" name="payment" value="ewallet" v-model="payment">
         <label class="choice__label" for="radio-ewallet">
           <p>
             <span class="text">e-Wallet</span><br>
@@ -52,14 +52,14 @@
         <span class="choice__check material-icons">check</span>
       </div>
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-bank" name="payment" value="bank">
+        <input type="radio" class="choice__input" id="radio-bank" name="payment" value="bank" v-model="payment">
         <label class="choice__label" for="radio-bank">
           <p>Bank Transfer</p>
         </label>
         <span class="choice__check material-icons">check</span>
       </div>
       <div class="choice">
-        <input type="radio" class="choice__input" id="radio-va" name="payment" value="va">
+        <input type="radio" class="choice__input" id="radio-va" name="payment" value="va" v-model="payment">
         <label class="choice__label" for="radio-va">
           <p>Virtual Account</p>
         </label>
@@ -71,6 +71,23 @@
 
 <script>
 export default {
-  name: 'Payment'
+  name: 'Payment',
+  data() {
+    return {
+      shipment: '',
+      payment: ''
+    }
+  },
+  watch: {
+    shipment: function (val) {
+      this.$store.commit('setShipment', val)
+    },
+    payment: function (val) {
+      this.$store.commit('setPayment', val)
+    }
+  },
+  mounted() {
+    this.$store.commit('setCurrentPage', 'payment')
+  }
 }
 </script>
